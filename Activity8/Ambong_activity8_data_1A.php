00712cbc823e10dfb5d333ccba92e8a67d5d8e01
JCNA9029 <103157443+JCNA9029@users.noreply.github.com>
2023-06-27 17:55:17 +0800

    Add files via upload

---
+++ Activity8/Ambong_activity8_data_1A.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "p0werp0int";
+$database = "basicform";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+$sql = "INSERT INTO aaaaa (name, email, message) VALUES (?, ?, ?)";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("sss", $name, $email, $message);
+
+if ($stmt->execute()) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$stmt->close();
+$conn->close();
+
+?>
